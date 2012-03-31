016e1dcf0d4755cd06533dc5937df4ade0216cdc
Chris Kinoti <mckinoti@gmail.com>
2012-03-31 09:44:28 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileMoney::Application.config.secret_token = '36e831d0bb937d4db0e0281f14ab9dac654d237aa914bd036c800bc4751bb3c02cfbb30cac144fd4b891e643da928f15d1c71f8c548cc260f2fb894ede7e3b5d'
